5df50eeb39b488c4456899072bd69bbc0d82639e
maiiix <53633944+maiiix@users.noreply.github.com>
2020-12-04 13:42:38 -0500

    update-app.js

--- App.js
+++ App.js
@@ -1,9 +1,14 @@
-import 'react-native-gesture-handler' 
+import 'react-native-gesture-handler'
+import { decode, encode } from 'base-64';global.crypto = require("@firebase/firestore");global.crypto.getRandomValues = byteArray => { for (let i = 0; i < byteArray.length; i++) { byteArray[i] = Math.floor(256 * Math.random()); } }
+if (!global.btoa) { global.btoa = encode; }
+if (!global.atob) { global.atob = decode; }
+
+
 
 import React from 'react'
 
 import MainStackNavigator from './src/Navigation/MainStackNavigator'
 
-export default function App() {
+export default function App(){
   return <MainStackNavigator/>
 }
