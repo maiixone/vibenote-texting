d5c6e4f6800d96513f8fad281b27ab1ef3c27256
maiiix <53633944+maiiix@users.noreply.github.com>
2020-12-04 11:16:50 -0500

    update-screens

--- src/screens/ChatScreen.js
+++ src/screens/ChatScreen.js
@@ -1,27 +1,178 @@
-import React from 'react'
-import {StyleSheet, View, Text} from 'react-native'
+import React, { useState, useEffect } from 'react'
+import { StyleSheet, View, TouchableOpacity, Text, FlatList, KeyboardAvoidingView, Alert } from 'react-native'
+import firebase, { firestore } from '../firebase/Firebase'
+import Strings from '../const/String'
+import DismissKeyboard from '../components/DismissKeyboard'
+import MessageItem from '../components/MessageItem'
+import MessageFieldView from '../components/MessageFieldView'
 
-function ChatScreen(){
-    return(
-       <View style={styles.container}>
-           <Text style={styles.text} >Vibe Chats</Text>
-           
-       </View>
+
+function ChatScreen({ route, navigation }) {
+    const [messageList, setMessageList] = useState([])
+    const [message, setMessage] = useState('')
+    const [isJoined, setIsJoined] = useState(false)
+
+    const { item } = route.params
+    const userID = firebase.auth().currentUser.uid
+
+    useEffect(() => {
+        console.log(item)
+        getUserJoinedAlreadyOrNot()
+        getMessages()
+    }, [])
+
+    function getUserJoinedAlreadyOrNot() {
+        firestore.collection("members").doc(item.groupID).collection("member").where("userID", "==", userID)
+            .get().then(function (querySnapshot) {
+                if (querySnapshot.size > 0) {
+                    querySnapshot.forEach(function (doc) {
+                        if (doc.data() != null) {
+                            setIsJoined(true)
+                        } else {
+                            setIsJoined(false)
+                            showAlertToJoinGroup()
+                        }
+                    })
+                } else {
+                    showAlertToJoinGroup()
+                }
+            }).catch(function (error) {
+                console.log("Error getting documents: ", error)
+            })
+    }
+
+    function showAlertToJoinGroup() {
+        Alert.alert(
+            Strings.JoinChat,
+            Strings.JoinChatConfirmMessage,
+            [{
+                text: 'Yes', onPress: () => {
+                    joinGroup()
+                }
+            }, {
+                text: 'No', onPress: () => {
+
+                }
+            }
+            ],
+            { cancelable: false }
+        )
+    }
+
+    function joinGroup() {
+        const groupMemberRef = firestore.collection("members").doc(item.groupID).collection('member').doc()
+        groupMemberRef.set({
+            userID: userID,
+        }).then(function (docRef) {
+            setIsJoined(true)
+            Alert.alert(Strings.joinMessage)
+            setMessage('')
+        }).catch(function (error) {
+            setIsJoined(false)
+            Alert.alert(Strings.JoinGroupError)
+        })
+    }
+
+    function getMessages() {
+        const db = firestore
+        var messages = []
+
+        db.collection("message").doc(item.groupID).collection("messages")
+            .onSnapshot(function (snapshot) {
+                snapshot.docChanges().forEach(function (change) {
+                    if (change.type === "added") {
+                        console.log("New Message: ", change.doc.data())
+                        messages.push(change.doc.data())
+
+                    }
+                    if (change.type === "modified") {
+                        console.log("Modified Message", change.doc.data())
+                    }
+                    if (change.type === "removed") {
+                        console.log("Removed Message:", change.doc.data())
+                    }
+                    setMessageList(messages)
+                })
+            })
+    }
+
+    function sendMessagesToChat() {
+        const messageRef = firestore.collection("message").doc(item.groupID).collection("messages").doc()
+        const userEmail = firebase.auth().currentUser.email
+
+        messageRef.set({
+            messageID: messageRef.id,
+            message: message,
+            senderId: userID,
+            senderEmail: userEmail
+        }).then(function (docRef) {
+            console.log("Document written with ID: ", messageRef.id)
+            setMessage('')
+        }).catch(function (error) {
+            Alert.alert(error.message)
+            console.log("Error:", error)
+        })
+
+    }
+
+
+
+    return (
+        <DismissKeyboard>
+            <KeyboardAvoidingView style={{ flex: 1, flexDirection: 'column', justifyContent: 'center', }}
+                behavior="padding" enabled keyboardVerticalOffset={100}>
+                <View style={styles.container}>
+                    <FlatList
+                        style={styles.flatList}
+                        data={messageList}
+                        keyExtractor={(item, index) => 'key' + index}
+                        renderItem={({ item }) => {
+                            return (
+                                <TouchableOpacity onPress={() => {
+
+                                }}>
+
+                                    <MessageItem item={item} />
+                                </TouchableOpacity>
+                            )
+                        }}
+                    />
+
+                    <View style={styles.messageFieldView}>
+                        <MessageFieldView term={message}
+                            placeHolder={Strings.typeYourMessage}
+                            onTermChange={message => setMessage(message)}
+                            onSubmit={sendMessagesToChat}
+                        >
+
+                        </MessageFieldView>
+
+                    </View>
+
+
+
+                </View>
+
+            </KeyboardAvoidingView>
+
+        </DismissKeyboard>
     )
 }
 
 const styles = StyleSheet.create({
-   container: {
-       flex: 1,
-       justifyContent: 'center',
-       alignItems: 'center',
-       backgroundColor: '#ebebeb',
-   },
-   text: {
-       color: '#101010',
-       fontSize: 24,
-       fontWeight: 'bold'
-   } 
+    container: {
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        flex: 1
+    },
+    flatList: {
+        marginBottom: 10,
+        flex: 0.9,
+    },
+    messageFieldView: {
+        flex: 0.1
+    }
 })
 
 export default ChatScreen
--- src/screens/GroupScreen.js
+++ src/screens/GroupScreen.js
@@ -1,27 +1,108 @@
-import React from 'react'
-import {StyleSheet, View, Text} from 'react-native'
+import React, { useLayoutEffect, useState, useEffect } from 'react'
+import { StyleSheet, View, Text, FlatList, TouchableOpacity, Alert } from 'react-native'
+import ButtonWithBackground from '../components/ButtonWithBackground'
+import GroupItem from '../components/GroupsItems'
+import Images from '../const/Images'
+import firebase, { firestore } from '../firebase/Firebase'
 
-function GroupScreen(){
-    return(
-       <View style={styles.container}>
-           <Text style={styles.text} >Group Vibes</Text>
-           
-       </View>
+function GroupsScreen({ navigation }) {
+    const [groups, setGroups] = useState([])
+
+    useLayoutEffect(() => {
+        navigation.setOptions({
+            headerRight: () => (
+                <ButtonWithBackground onPress={() => {
+                    navigation.navigate('Add Group Screen')
+                }}
+                    image={Images.add}
+                />
+            ),
+            headerLeft: () => (
+                <ButtonWithBackground onPress={() => {
+                    signOutUser()
+                }}
+                    image={Images.logout}
+                />
+            )
+        })
+
+    })
+
+    signOutUser = async () => {
+        try{
+            await firebase.auth().signOut()
+            // navigation.reset({
+            //     index: 0,
+            //     routes: [{name: 'SplashScreen'}]
+            // })
+        }catch(e){
+            console.log(e)
+        }
+    }
+
+    useEffect(() => {
+        getChats()
+    }, [])
+
+    function getChats() {
+        const db = firestore
+        var groupArray = []
+
+        db.collection("groups")
+            .onSnapshot(function (snapshot) {
+                snapshot.docChanges().forEach(function (change) {
+                    if (change.type == "added") {
+                        console.log("New Group: ", change.doc.data())
+                        groupArray.push(change.doc.data())
+                    }
+                    if (change.type === "modified") {
+                        console.log("Modified Group: ", change.doc.data())
+                    }
+                    if (change.type === "removed") {
+                        console.log("Removed Group", change.doc.data())
+                    }
+
+                    setGroups(groupArray)
+                })
+            })
+
+    }
+
+
+    return (
+        <View style={styles.container}>
+            <FlatList
+                data={groups}
+                keyExtractor={(item, index) => 'key' + index}
+                renderItem={({ item }) => {
+                    return (
+                        <TouchableOpacity onPress={() => {
+                            navigation.navigate('Chat Screen', {
+                                item
+                            })
+                        }}>
+                            <GroupItem item={item}></GroupItem>
+                        </TouchableOpacity>
+                    )
+                }}
+            >
+            </FlatList>
+        </View>
     )
 }
 
 const styles = StyleSheet.create({
-   container: {
-       flex: 1,
-       justifyContent: 'center',
-       alignItems: 'center',
-       backgroundColor: '#ebebeb',
-   },
-   text: {
-       color: '#101010',
-       fontSize: 24,
-       fontWeight: 'bold'
-   } 
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#ebebeb'
+    },
+    text: {
+        color: '#101010',
+        fontSize: 24,
+        fontWeight: 'bold'
+    }
 })
 
-export default GroupScreen
+export default GroupsScreen
